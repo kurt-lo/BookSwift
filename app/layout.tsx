a34ad39c5af0e7548f0eba185ec800d7a9fb6104
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2024-01-06 13:00:52 +0800

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,7 +20,6 @@ export default function RootLayout({
       <body>
       <Navbar />
         {children}
-      <Footer />
       </body>
     </html>
   )
